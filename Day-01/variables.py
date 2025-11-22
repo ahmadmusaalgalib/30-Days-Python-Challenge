ec8ac78db4a94237465a0261bd7b899e10c09f2d
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-11-23 00:12:08 +0600

    first commit

---
+++ Day-01/variables.py
@@ -0,0 +1,42 @@
+#variables
+'''
+this is a simple python program to demonstrate variables
+
+name ='ahmed musa al galib'
+age = 22
+height = 5.12
+print(name, age, height)
+print(type(name))
+print(type(age))    
+print(type(height))
+
+
+# list
+
+my_info=['name','age','height']
+print(my_info)
+
+#tuple
+
+my_info2=('name','age','height')
+print(my_info2)
+
+#set
+my_set={'name','age','height'}
+print(my_set)
+
+#mao
+my_map={
+    "name":"musa",
+    "age": 22
+}
+
+print (my_map)
+
+#frozenset
+
+x= frozenset({"apple", "banana", "cherry"})
+print(x)
+'''
+
+print(input("What is your name?\n")+ " Hello")
