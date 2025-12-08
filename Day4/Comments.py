233cc9700040fd17d412f82aceaa36b9b8acbba9
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-09 05:48:11 +0600

    day4

---
+++ Day4/Comments.py
@@ -0,0 +1,14 @@
+'''
+Single-line comment → starts with #
+
+# This is a comment
+
+
+Multi-line comment → use triple quotes ''' ''' or """ """
+
+"""
+This is a
+multi-line comment
+"""
+
+'''
---
+++ Day4/Operator_precedence.py
@@ -0,0 +1,8 @@
+result=2+3*5
+print(result)
+
+result2=(2+3)-(2+5)
+print(result2)
+
+result3=(4*3)//(2+4)
+print(result3)
