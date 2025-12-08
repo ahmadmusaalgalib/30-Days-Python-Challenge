f69b59e0f44513f9bdcece1e4674a68db7c74c04
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-09 04:55:20 +0600

    Arithmetic Operator

---
+++ Day4/Arithmetic_Operator.py
@@ -0,0 +1,18 @@
+#Arithmetic_Operator(+,-,*,/,%)
+
+#for positive
+print(5+2)
+#for negative
+print(5-2)
+#for multiplication
+print(5*2)
+#for Divide
+print(5/2)
+'''
+some case we don't need floating value in 
+that time we use(//)for integer value
+'''
+print(5//2)
+
+#for modulo or reminder
+print(5%2)
