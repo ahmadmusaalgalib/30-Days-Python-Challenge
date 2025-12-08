c7565375b0fc9c416aeaa5a862ddb61ea52770d4
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-09 04:29:01 +0600

    String

---
+++ Day3/Strings.py
@@ -0,0 +1,9 @@
+name = "Ahmed Musa"
+
+#for uppercase
+print(name.upper())
+#for lowercase
+print(name.lower())
+#replace the string or charceter
+print(name.replace("Ahmed Musa", "AL Galib"))
+print(name.find("Musa"))
