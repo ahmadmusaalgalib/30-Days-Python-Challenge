a465084d636cd3572fefa330fba65c2ec3b7e659
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-17 23:23:33 +0600

    Create Function.py

---
+++ Day13/Function.py
@@ -0,0 +1,32 @@
+'''
+1)In-Built Functions
+     int(),
+     str(),
+     bool()
+..................
+2)Module Functions
+    like math function
+        import math
+        or
+        from math import "which module need"
+        or if you need all module then: 
+        from math import *
+
+3)User-Defined Functions
+    for this function there have a syntax:
+        
+        def function_name(parameters):
+            //do something 
+
+
+'''
+
+from math import*
+
+print(sqrt(4))
+
+import numpy as np
+
+v = np.array([1, 2, 3])
+print(v * 2)
+
