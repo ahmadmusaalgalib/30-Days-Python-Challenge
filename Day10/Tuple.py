6dfda7709f9bac5d5cd835dd9403d9896e712b49
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-17 22:23:34 +0600

    d

--- Day10/Tuple.py
+++ Day10/Tuple.py
@@ -1,5 +1,9 @@
 marks=(88,93,22,88,22,37)
+students="musa","Zimia","galib","Salma"
 
-print(marks)
+print(students)
+print(*students)
+
+print(*marks)
 print(marks.count(88))
 print(marks.index(37))
---
+++ Day11/set.py
@@ -0,0 +1,7 @@
+marks={8,93,22,88,22,37}
+
+
+print(marks)
+
+for socre in marks:
+    print(socre)
